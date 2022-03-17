b8a86e93bd27699781b40e07cd737ab822402176
glima <glima20@gmail,com>
2022-03-18 00:03:42 +0100

    fix test

--- pipeline/train_model.py
+++ pipeline/train_model.py
@@ -36,9 +36,7 @@ def main() -> None:
         configure_mlflow(MLFLOW_EXPERIMENT)
         data = download_dataset(DATA_URL)
         features, labels = pre_process_data(data)
-        print(features)
-        print(labels)
-        #train_model(features, labels)
+        train_model(features, labels)
     except Exception as e:
         msg = f"training stage failed with exception: {e}"
         log.error(msg)
