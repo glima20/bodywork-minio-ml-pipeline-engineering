5dcccf822f8b52c4af7d498f2d89a54c018aa56d
glima <glima20@gmail,com>
2022-03-17 23:52:03 +0100

    fix test

--- pipeline/train_model.py
+++ pipeline/train_model.py
@@ -1,118 +1,126 @@
 """
-This module defines what will happen in 'stage-1-train-model':
+This module defines what will happen in the 'train-model' stage:
 
 - download dataset;
 - pre-process data into features and labels;
 - train machine learning model; and,
-- save model to cloud stirage (AWS S3).
+- log and register the model with MLflow.
 """
-from datetime import datetime
 from urllib.request import urlopen
+from subprocess import CalledProcessError, run
 from typing import Tuple
 
-import boto3 as aws
+import mlflow
 import numpy as np
 import pandas as pd
-from joblib import dump
 from sklearn.base import BaseEstimator
 from sklearn.metrics import f1_score, balanced_accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 
-DATA_URL = ('http://bodywork-ml-pipeline-project.s3.eu-west-2.amazonaws.com'
-            '/data/iris_classification_data.csv')
-TRAINED_MODEL_AWS_BUCKET = 'bodywork-ml-pipeline-project'
-TRAINED_MODEL_FILENAME = 'iris_tree_classifier.joblib'
+from utils import configure_logger, configure_mlflow
+
+MLFLOW_EXPERIMENT = "iris-classifier"
+MLFLOW_MODEL_NAME = f"{MLFLOW_EXPERIMENT}--sklearn-decision-tree"
+DATA_URL = (
+    "http://bodywork-ml-pipeline-project.s3.eu-west-2.amazonaws.com"
+    "/data/iris_classification_data.csv"
+)
+
+log = configure_logger()
 
 
 def main() -> None:
     """Main script to be executed."""
-    data = download_dataset(DATA_URL)
-    print("data: {}".format(len(data)))
-    #features, labels = pre_process_data(data)
-    #trained_model = train_model(features, labels)
-    #persist_model(trained_model)
-    print("DONE")
+    try:
+        configure_mlflow(MLFLOW_EXPERIMENT)
+        data = download_dataset(DATA_URL)
+        features, labels = pre_process_data(data)
+        train_model(features, labels)
+    except Exception as e:
+        msg = f"training stage failed with exception: {e}"
+        log.error(msg)
+        raise RuntimeError(msg)
 
 
 def download_dataset(url: str) -> pd.DataFrame:
     """Get data from cloud object storage."""
-    print(f'downloading training data from {DATA_URL}')
+    log.info(f"Downloading training data from {DATA_URL}.")
     data_file = urlopen(url)
     return pd.read_csv(data_file)
 
 
 def pre_process_data(data: pd.DataFrame) -> Tuple[np.ndarray, np.ndarray]:
     """Prepare raw data for model training."""
-    label_column = 'species'
+    label_column = "species"
     feature_columns = [
-        'sepal length (cm)',
-        'sepal width (cm)',
-        'petal length (cm)',
-        'petal width (cm)'
+        "sepal length (cm)",
+        "sepal width (cm)",
+        "petal length (cm)",
+        "petal width (cm)",
     ]
-    classes_map = {'setosa': 0, 'versicolor': 1, 'virginica': 2}
+    classes_map = {"setosa": 0, "versicolor": 1, "virginica": 2}
     X = data[feature_columns].values
     y = data[label_column].apply(lambda e: classes_map[e]).values
     return X, y
 
 
-def log_model_metrics_to_stdout(
-    y_actual: np.ndarray,
-    y_predicted: np.ndarray
-) -> None:
-    """Print model evaluation metrics to stdout."""
-    time_now = datetime.now().isoformat(timespec='seconds')
-    accuracy = balanced_accuracy_score(
-        y_actual,
-        y_predicted,
-        adjusted=True
-    )
-    f1 = f1_score(
-        y_actual,
-        y_predicted,
-        average='weighted'
-    )
-    print(f'iris model metrics @{time_now}')
-    print(f' |-- accuracy = {accuracy:.3f}')
-    print(f' |-- f1 = {f1:.3f}')
+def log_model_metrics(y_actual: np.ndarray, y_predicted: np.ndarray) -> None:
+    """log.info model evaluation metrics to stdout."""
+    accuracy = balanced_accuracy_score(y_actual, y_predicted, adjusted=True)
+    f1 = f1_score(y_actual, y_predicted, average="weighted")
+    mlflow.log_metric("accuracy", accuracy)
+    mlflow.log_metric("f1", f1)
 
 
-def train_model(features: np.ndarray, labels: np.ndarray) -> BaseEstimator:
-    """Train ML model."""
-    X_train, X_test, y_train, y_test = train_test_split(
-        features,
-        labels,
-        test_size=0.1,
-        stratify=labels,
-        random_state=42
-    )
-    print('training iris decision tree classifier')
-    iris_tree_classifier = DecisionTreeClassifier(
-        class_weight='balanced',
-        random_state=42
-    )
-    iris_tree_classifier.fit(X_train, y_train)
-    test_data_predictions = iris_tree_classifier.predict(X_test)
-    log_model_metrics_to_stdout(y_test, test_data_predictions)
-    return iris_tree_classifier
-
-
-def persist_model(model: BaseEstimator) -> None:
-    """Put trained model into cloud object storage."""
-    dump(model, TRAINED_MODEL_FILENAME)
+def get_pipeline_git_commit_hash() -> str:
+    """The project's git commit to use as a versioning tag."""
     try:
-        s3_client = aws.client('s3')
-        s3_client.upload_file(
-            TRAINED_MODEL_FILENAME,
-            TRAINED_MODEL_AWS_BUCKET,
-            f'models/{TRAINED_MODEL_FILENAME}'
+        git_cmd = run(
+            ["git", "rev-parse", "--short", "HEAD"],
+            capture_output=True,
+            check=True,
+            encoding="utf-8",
         )
-        print(f'model saved to s3://{TRAINED_MODEL_AWS_BUCKET}'
-              f'/{TRAINED_MODEL_FILENAME}')
-    except Exception:
-        print('could not upload model to S3 - check AWS credentials')
+    except CalledProcessError:
+        raise RuntimeError("Could not get Git commit hash.")
+    return git_cmd.stdout
 
 
-if __name__ == '__main__':
+def train_model(features: np.ndarray, labels: np.ndarray) -> None:
+    """Train ML model and register with MLflow."""
+    random_state = np.random.randint(0, 100)
+    run_name = f"pipeline-{get_pipeline_git_commit_hash()}"
+    with mlflow.start_run(run_name=run_name) as training_run:
+        X_train, X_test, y_train, y_test = train_test_split(
+            features, labels, test_size=0.2, stratify=labels, random_state=random_state
+        )
+
+        log.info("Training iris decision tree classifier.")
+        mlflow.log_param("random_state", random_state)
+        iris_tree_classifier = DecisionTreeClassifier(
+            class_weight="balanced", random_state=random_state
+        )
+        iris_tree_classifier.fit(X_train, y_train)
+        test_data_predictions = iris_tree_classifier.predict(X_test)
+        log_model_metrics(y_test, test_data_predictions)
+
+        log.info("Registering new model with MLflow.")
+        mlflow.sklearn.log_model(
+            sk_model=iris_tree_classifier, artifact_path=MLFLOW_MODEL_NAME
+        )
+        new_model_metadata = mlflow.register_model(
+            model_uri=f"runs:/{training_run.info.run_id}/{MLFLOW_MODEL_NAME}",
+            name=MLFLOW_MODEL_NAME,
+        )
+
+        log.info("Transitioning new model to production.")
+        mlflow.tracking.MlflowClient().transition_model_version_stage(
+            name=MLFLOW_MODEL_NAME,
+            version=int(new_model_metadata.version),
+            stage="Production",
+        )
+
+
+if __name__ == "__main__":
     main()
